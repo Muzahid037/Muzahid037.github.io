d8953bbec45c2d848adf7f3ef47f9b2fbd9bee77
Md. Muzahidul Islam <48602232+Muzahid037@users.noreply.github.com>
2025-07-10 00:01:38 +0600

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h2>hello...</h2>
+      <h1 className="bg-red-800">
+        This muzahid
+        </h1> 
+    </div>
+  );
+}
